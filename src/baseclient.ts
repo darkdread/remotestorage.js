6016c9425a1a5d758f70eb806140afca9b89d8c2
Sebastian Kippe <sebastian@kip.pe>
2022-03-22 17:59:17 -0600

    Add helper function for deriving module names

--- src/baseclient.ts
+++ src/baseclient.ts
@@ -9,6 +9,11 @@ import config from './config';
 import { applyMixins, cleanPath, isFolder } from './util';
 import RemoteStorage from './remotestorage';
 
+function getModuleNameFromBase(path: string): string {
+  const parts = path.split('/');
+  return path.length > 2 ? parts[1] : 'root';
+}
+
 /**
  * Provides a high-level interface to access data below a given root path.
  */
@@ -42,16 +47,7 @@ class BaseClient {
 
     this.storage = storage;
     this.base = base;
-
-    /**
-     * TODO: document
-     */
-    const parts = this.base.split('/');
-    if (parts.length > 2) {
-      this.moduleName = parts[1];
-    } else {
-      this.moduleName = 'root';
-    }
+    this.moduleName = getModuleNameFromBase(this.base);
 
     this.addEvents(['change']);
     this.on = this.on.bind(this);
